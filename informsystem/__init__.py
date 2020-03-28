c2bf4c65775d5c28e88d90ff7ed457dd37f5367d
ST Liar <bohdansapovsky@gmail.com>
2020-03-28 17:20:21 +0200

    #12

--- informsystem/__init__.py
+++ informsystem/__init__.py
@@ -5,6 +5,25 @@ import flask_excel as excel
 
 
 def create_app(test_config=None):
+    from logging.config import dictConfig
+
+
+    dictConfig({
+        'version': 1,
+        'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+        'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+        'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+    })
+
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
